5494427a35bdb66a25f66575e67a7de100029c48
Moin Shah <smoinx69@gmail.com>
2026-07-30 19:28:09 +0500

    Delete fix_app.ps1

--- fix_app.ps1
+++
@@ -1,3 +0,0 @@
-﻿\ = Get-Content "C:\Users\Administrator\Documents\DATABASE PROJECT\Gaming Tournament Management System\src\app\App.tsx" -Raw
-\ = \ -replace '\\"', '"'
-Set-Content -Path "C:\Users\Administrator\Documents\DATABASE PROJECT\Gaming Tournament Management System\src\app\App.tsx" -Value \ -Encoding utf8
